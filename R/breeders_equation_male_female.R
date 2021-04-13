73852628453dbe1aa256a5b70bc635e1b48a0a89
NeilHobbs <neilphobbs@hotmail.com>
2021-04-13 17:22:31 +0100

    error message included

--- R/breeders_equation_male_female.R
+++ R/breeders_equation_male_female.R
@@ -9,6 +9,8 @@ breeders_equation_male_female = function(heritability,
                                          female.selection.differential,
                                          male.selection.differential){
 
+  if(0 > heritability |heritability > 1){stop("heritability must be between 0 and 1")}
+
   response = ((heritability/2) * female.selection.differential) + ((heritability/2) * male.selection.differential)
 
   return(response)
