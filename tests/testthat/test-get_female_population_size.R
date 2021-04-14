83711a2fb12be4dff49d0295f5d301d887a69d94
NeilHobbs <neilphobbs@hotmail.com>
2021-04-14 17:08:09 +0100

    not needed

--- tests/testthat/test-get_female_population_size.R
+++
@@ -1,9 +0,0 @@
-test_that("population size unaffected by trait.mean", {
-  expect_equal(get_female_total_population_size(relative.contributions.before.selection = calculate_density_of_trait_values(vector.length = 1000,
-                                                                                                                     trait.mean = 100,
-                                                                                                                     standard.deviation = 10)),
-               get_female_total_population_size(relative.contributions.before.selection = calculate_density_of_trait_values(vector.length = 1000,
-                                                                                                                     trait.mean = 100000000,
-                                                                                                                     standard.deviation = 10)))
-})
-
