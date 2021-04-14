dcb9f39ba5c7bd38cbddf25d95ecfc71fcf86ab4
NeilHobbs <neilphobbs@hotmail.com>
2021-04-14 13:19:08 +0100

    added error messages

--- R/number_migrating_refugia_to_intervention.R
+++ R/number_migrating_refugia_to_intervention.R
@@ -6,7 +6,10 @@
 number_migrating_refugia_to_intervention = function(dispersal.rate,
                                                     intervention.coverage){
 
-  migration.refugia.to.intervention =  dispersal.rate * (1 - intervention.coverage)
+  if(dispersal.rate > 1 | dispersal.rate < 0){stop("dipersal.rate must be between 0 and 1")}
+  if(intervention.coverage > 1 | intervention.coverage < 0){stop("intervention.coverage must be between 0 and 1")}
+
+    migration.refugia.to.intervention =  dispersal.rate * (1 - intervention.coverage)
 
   return(migration.refugia.to.intervention)
 }
