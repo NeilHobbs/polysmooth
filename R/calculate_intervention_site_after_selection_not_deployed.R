8514c8651b386ca8d5da48227c84716c43fdd0bf
NeilHobbs <neilphobbs@hotmail.com>
2021-04-14 13:00:03 +0100

    Corrected the title

--- R/calculate_intervention_site_after_selection_not_deployed.R
+++ R/calculate_intervention_site_after_selection_not_deployed.R
@@ -1,4 +1,4 @@
-#'@titleThe Calculate the mean Polygenic Resistance Score of the mosquito population in the intervention site after  fitness cost based selection.
+#'@title Calculate the mean Polygenic Resistance Score of the mosquito population in the intervention site after  fitness cost based selection.
 #'
 #'@param intervention.before.selection = The mean Polygenic Resistance Score of the mosquito population in the intervention before selection has occurred that generation.
 #'@param response.fitness = The response to selection from fitness costs only, such as would occur when the insecticide is either not deployed in the intervention site or the selection that occurs in the refugia.
