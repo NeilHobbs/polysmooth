61eb043e7f32c73e1ad4551d40391f3ddb445d6d
NeilHobbs <neilphobbs@hotmail.com>
2021-04-14 13:20:50 +0100

    added parameter descriptions

--- R/wrapper_calculate_male_insecticide_fitness_selection_differential.R
+++ R/wrapper_calculate_male_insecticide_fitness_selection_differential.R
@@ -1,17 +1,17 @@
 #'@title A wrapper function to calculate the male insecticide and fitness cost selection differential
 #'
-#'@param male.trait.mean =
-#'@param female.insecticide.exposure =
-#'@param male.insecticide.exposure =
-#'@param standard.deviation =
-#'@param vector.length =
-#'@param maximum.bioassay.survival.proportion =
-#'@param michaelis.menten.slope =
-#'@param half.population.bioassay.survival.resistance =
-#'@param regression.coefficient =
-#'@param regression.intercept =
-#'@param current.insecticide.efficacy =
-#'@param exposure.scaling.factor =
+#'@param male.trait.mean = The mean Polygenic Resistance Score to the insecticide for male mosquitoes before selection has occurred.
+#'@param female.insecticide.exposure =Proportion of female mosquitoes in the intervention site that encounter and are exposed to the deployed insecticide.
+#'@param male.insecticide.exposure = Proportion of male mosquitoes in the intervention site that encounter and are exposed to the deployed insecticide as a proportion of the exposure of female mosquitoes
+#'@param standard.deviation = The standard deviation of the trait mean in the population.
+#'@param vector.length = The length of the vector to be returned. A minimum value of 100,000 is recommmended.
+#'@param maximum.bioassay.survival.proportion = The maximum proportion of mosquitoes that can survive in the bioassay.
+#'@param michaelis.menten.slope = The slope in the Michaelis-Menten equation.
+#'@param half.population.bioassay.survival.resistance = The Polygenic Resistance Score which gives a 50% survival probability in a WHO cylinder bioassay.
+#'@param regression.coefficient = A linear model coefficient obtained from performing a linear model on paired experimental hut trials and WHO cylinder bioassays.
+#'@param regression.intercept = The linear model intercept obtained from performing a linear model on paired experimental hut trials and WHO cylinder bioassays.
+#'@param current.insecticide.efficacy = The current efficacy of the insecticide, defined as proportion of fully susceptible mosquitoes surviving contact with the insecticide-treated surface.
+#'@param exposure.scaling.factor = A factor which converts the insecticide exposure to the selection differential.
 
 
 
@@ -19,6 +19,7 @@ wrapper_calculate_male_insecticide_fitness_selection_differential = function(mal
                                                                              female.insecticide.exposure,
                                                                              male.insecticide.exposure,
                                                                              standard.deviation,
+                                                                             male.fitness.cost,
                                                                              vector.length,
                                                                              maximum.bioassay.survival.proportion,
                                                                              michaelis.menten.slope,
