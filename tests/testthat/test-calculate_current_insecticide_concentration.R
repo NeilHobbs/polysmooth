64a396902feaa2ffcd77f41e4f05fff7278e4eba
NeilHobbs <neilphobbs@hotmail.com>
2021-04-14 17:10:23 +0100

    not needed

--- tests/testthat/test-calculate_current_insecticide_concentration.R
+++
@@ -1,5 +0,0 @@
-test_that("multiplication works", {
-  expect_equal(calculate_current_insecticide_concentration(applied.concentration,
-                                                           instantaneous.decay.rate,
-                                                           generations.since.deployment), 4)
-})
