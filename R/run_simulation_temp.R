eeaecf1ed7e150f8e2c285843957c8630fe9ead7
NeilHobbs <neilphobbs@hotmail.com>
2021-04-15 15:16:51 +0100

    appears to be currently working

--- R/run_simulation_temp.R
+++ R/run_simulation_temp.R
@@ -1,11 +1,14 @@
+#currently appears to be working...
 
 run_simulation_temp = function(number.of.insecticides = 2,
                                exposure.scaling.factor = 10,
-                               nsim = 1000,
                                female.fitness.cost,
                                male.fitness.cost,
                                female.insecticide.exposure,
                                male.insecticide.exposure,
+                               heritability,
+                               dispersal.rate,
+                               intervention.coverage,
                                standard.deviation,
                                vector.length,
                                maximum.bioassay.survival.proportion,
@@ -20,7 +23,10 @@ run_simulation_temp = function(number.of.insecticides = 2,
                                deployment.frequency = 10, #Number of mosquito generations between choosing insecticides (note, 1 year is 10 generations)
                                maximum.resistance.value = 25000,
                                starting.refugia.resistance.score = 0,
-                               starting.intervention.resistance.score = 0){
+                               starting.intervention.resistance.score = 0,
+                               insecticide.population.suppression, #need to incorporate this into the model directly
+                               current.insecticide.efficacy #neeed to incorporate this intot the model directly
+                                ){
 
   #Start by creating an array (calls the array_named function):
   #dimension 1: site = c("refugia", "treatment"), which hold resistance intensities.
@@ -103,25 +109,25 @@ run_simulation_temp = function(number.of.insecticides = 2,
 
         if(insecticide == deployed.insecticide[generation]){
 
-          tracked.resistance = wrapper_intervention_site_after_selection_deployed(insecticide.population.suppression = insecticide.population.suppression,
-                                                                                  intervention.before.selection = sim.array['intervention', insecticide, generation-1],
-                                                                                  female.fitness.cost = female.fitness.cost,
-                                                                                  male.fitness.cost = male.fitness.cost,
-                                                                                  female.insecticide.exposure = female.insecticide.exposure,
-                                                                                  male.insecticide.exposure = male.insecticide.exposure,
-                                                                                  standard.deviation = standard.deviation,
-                                                                                  vector.length = vector.length,
-                                                                                  maximum.bioassay.survival.proportion = maximum.bioassay.survival.proportion,
-                                                                                  michaelis.menten.slope = michaelis.menten.slope,
-                                                                                  half.population.bioassay.survival.resistance = half.population.bioassay.survival.resistance,
-                                                                                  regression.coefficient = regression.coefficient,
-                                                                                  regression.intercept = regression.intercept,
-                                                                                  current.insecticide.efficacy = current.insecticide.efficacy,
-                                                                                  exposure.scaling.factor = exposure.scaling.factor,
-                                                                                  heritability = heritability,
-                                                                                  refugia.before.selection = sim.array['refugia', insecticide, generation-1],
-                                                                                  dispersal.rate = dispersal.rate,
-                                                                                  intervention.coverage = intervention.coverage)
+          tracked.resistance = wrapper_intervention_refugia_deployed_dispersal(insecticide.population.suppression = insecticide.population.suppression,
+                                                                               intervention.before.selection = sim.array['intervention', insecticide, generation-1],
+                                                                               female.fitness.cost = female.fitness.cost,
+                                                                               male.fitness.cost = male.fitness.cost,
+                                                                               female.insecticide.exposure = female.insecticide.exposure,
+                                                                               male.insecticide.exposure = male.insecticide.exposure,
+                                                                               standard.deviation = standard.deviation,
+                                                                               vector.length = vector.length,
+                                                                               maximum.bioassay.survival.proportion = maximum.bioassay.survival.proportion,
+                                                                               michaelis.menten.slope = michaelis.menten.slope,
+                                                                               half.population.bioassay.survival.resistance = half.population.bioassay.survival.resistance,
+                                                                               regression.coefficient = regression.coefficient,
+                                                                               regression.intercept = regression.intercept,
+                                                                               current.insecticide.efficacy = current.insecticide.efficacy,
+                                                                               exposure.scaling.factor = exposure.scaling.factor,
+                                                                               heritability = heritability,
+                                                                               refugia.before.selection = sim.array['refugia', insecticide, generation-1],
+                                                                               dispersal.rate = dispersal.rate,
+                                                                               intervention.coverage = intervention.coverage)
 
           sim.array['intervention', insecticide, generation] = tracked.resistance[[1]]
           sim.array['refugia', insecticide, generation] = tracked.resistance[[2]]
@@ -130,14 +136,14 @@ run_simulation_temp = function(number.of.insecticides = 2,
 
         if(insecticide != deployed.insecticide[generation]){
 
-          tracked.resistance = wrapper_intervention_site_not_deployed_dispersal(insecticide.population.suppression = insecticide.population.suppression,
-                                                                                intervention.before.selection = sim.array['intervention', insecticide, generation-1],
-                                                                                female.fitness.cost = female.fitness.cost,
-                                                                                male.fitness.cost = male.fitness.cost,
-                                                                                heritability = heritability,
-                                                                                refugia.before.selection = sim.array['refugia', insecticide, generation-1],
-                                                                                dispersal.rate = dispersal.rate,
-                                                                                intervention.coverage = intervention.coverage)
+          tracked.resistance = wrapper_intervention_refugia_not_deployed_dispersal(insecticide.population.suppression = insecticide.population.suppression,
+                                                                                   intervention.before.selection = sim.array['intervention', insecticide, generation-1],
+                                                                                   female.fitness.cost = female.fitness.cost,
+                                                                                   male.fitness.cost = male.fitness.cost,
+                                                                                   heritability = heritability,
+                                                                                   refugia.before.selection = sim.array['refugia', insecticide, generation-1],
+                                                                                   dispersal.rate = dispersal.rate,
+                                                                                   intervention.coverage = intervention.coverage)
 
           sim.array['intervention', insecticide, generation] = tracked.resistance[[1]]
           sim.array['refugia', insecticide, generation] = tracked.resistance[[2]]
@@ -148,39 +154,33 @@ run_simulation_temp = function(number.of.insecticides = 2,
 
         }#end of for insecticide loop
 
-      #returns the mean population insecticide resistance each generation.
-
-
-      #Which irm.strategy is being used: sequence or rotation
-
-      #May be worth making the following chunk of code into its own function as it is a bit chunky
-      #at the moment.
-      #Update insecticide each time the deployment.frequency is reached:
       if(generation < maximum.generations){
-        update.insecticide.info = if(generation %% deployment.frequency == 0){
-          if(irm.strategy == "rotation"){
-            irm_strategy_rotation(number.of.insecticides = number.of.insecticides,
-              current.generation = generation,
-              withdrawal.threshold = calc.withdrawal.threshold,
-              return.threshold = calc.return.threshold,
-              simulation.array = sim.array,
-              available.vector = available.vector,
-              withdrawn.vector = withdrawn.vector,
-              current.insecticide = deployed.insecticide[generation],
-              deployment.frequency = deployment.frequency,
-              deployment.vector = deployed.insecticide)}
-
-          if(irm.strategy == "sequence"){
-                  irm_strategy_sequence(number.of.insecticides = number.of.insecticides,
-                    current.generation = generation,
-                    withdrawal.threshold = calc.withdrawal.threshold,
-                    return.threshold = calc.return.threshold,
-                    simulation.array = sim.array,
-                    available.vector = available.vector,
-                    withdrawn.vector = withdrawn.vector,
-                    current.insecticide = deployed.insecticide[generation],
-                    deployment.frequency = deployment.frequency,
-                    deployment.vector = deployed.insecticide)
+        if(generation < maximum.generations){
+          update.insecticide.info = if(generation %% deployment.frequency == 0){
+            if(irm.strategy == "rotation"){
+              irm_strategy_rotation(
+                number.of.insecticides = number.of.insecticides,
+                current.generation = generation,
+                withdrawal.threshold = calc.withdrawal.threshold,
+                return.threshold = calc.return.threshold,
+                simulation.array = sim.array,
+                available.vector = available.vector,
+                withdrawn.vector = withdrawn.vector,
+                current.insecticide = deployed.insecticide[generation],
+                deployment.frequency = deployment.frequency,
+                deployment.vector = deployed.insecticide)} else{
+                  if(irm.strategy == "sequence"){
+                    irm_strategy_sequence(
+                      number.of.insecticides = number.of.insecticides,
+                      current.generation = generation,
+                      withdrawal.threshold = calc.withdrawal.threshold,
+                      return.threshold = calc.return.threshold,
+                      simulation.array = sim.array,
+                      available.vector = available.vector,
+                      withdrawn.vector = withdrawn.vector,
+                      current.insecticide = deployed.insecticide[generation],
+                      deployment.frequency = deployment.frequency,
+                      deployment.vector = deployed.insecticide)
 
                 }
               }
@@ -196,6 +196,8 @@ run_simulation_temp = function(number.of.insecticides = 2,
       #A break point to stop simuation if there is no insecticide deployed
       #if(is.na(deployed.insecticide[generation])){break}
 
+    }
+      }
     }
       }#end of for(generation) loop
 
