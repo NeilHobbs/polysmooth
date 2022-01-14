53a30baa346c77664832e06dacd94508b39963a2
NeilHobbs <neilphobbs@hotmail.com>
2022-01-14 10:27:06 +0000

    updated unit test for updated function

--- tests/testthat/test-create_insecticide_parameters_dataframe.R
+++ tests/testthat/test-create_insecticide_parameters_dataframe.R
@@ -3,7 +3,8 @@ temp.df = create_insecticide_parameters_dataframe(number.of.insecticides = 4,
                                         recommended.insecticide.dose = 1,
                                         threshold.generation = 1,
                                         base.efficacy.decay.rate = 4,
-                                        rapid.decay.rate = 5)
+                                        rapid.decay.rate = 5,
+                                        heritability = c(0.1, 0.2, 0.3, 0.3))
 
 test_that("correct number of rows", {
   expect_equal(nrow(temp.df), 4)
@@ -11,7 +12,7 @@ test_that("correct number of rows", {
 
 
 test_that("correct number of columns", {
-  expect_equal(ncol(temp.df), 6)
+  expect_equal(ncol(temp.df), 7)
 })
 
 
@@ -27,4 +28,8 @@ test_that("unique insecticides", {
 
 test_that("threshold generation the same for all", {
   expect_equal(temp.df$threshold.generations, c(1, 1, 1, 1))
+})
+
+test_that("specified heritabilities", {
+  expect_equal(temp.df$heritability, c(0.1, 0.2, 0.3, 0.3))
 })
