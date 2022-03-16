33ee19fa301625601ee07d87a9056b35f12b6809
NeilHobbs <neilphobbs@hotmail.com>
2022-03-16 16:29:04 +0000

    corrected endless while loop

--- R/choose_next_mixture.R
+++ R/choose_next_mixture.R
@@ -1,7 +1,7 @@
 #' @title Choose the next mixture.
-#' 
+#'
 #' @description Choose the next available mixture for deployment.
-#' 
+#'
 #' @param previous.mixture = The mixture that was last deployed
 #' @param total.mixtures = The total number of mixtures in the simulation
 #' @param available.mixtures = The mixtures that are available for deployment (both parts of the mixture are in the armoury)
@@ -20,17 +20,19 @@ choose_next_mixture = function(previous.mixture,
                                available.mixtures){
   #This will choose the mixture ID
     candidate.mixture = (previous.mixture + 1) #move to the next mixture
-  
-  
+
+
   `%notin%` = Negate(`%in%`) #make an "is not in" operator.
-  
+  if(candidate.mixture %in% available.mixtures){return(candidate.mixture)}else(
+
   while(candidate.mixture %notin% available.mixtures){
     if(candidate.mixture > total.mixtures){candidate.mixture = min(available.mixtures)}#go back to the start of the sequence
-    else(candidate.mixture = (candidate.mixture + 1))#try to go to the next available insecticide
+    else(candidate.mixture = (candidate.mixture + 1))
+         if(candidate.mixture %in% available.mixtures){return(candidate.mixture)}#try to go to the next available insecticide
+
   }
-  
-  return(candidate.mixture) #will return the ID of the candidate mixture 
-  
+)
+
 }
 
 
